c0c074a0e2dc100debc6e282ff624fb438d5aa26
Ilias <noskovill01@gmail.com>
2022-06-25 22:16:50 +0300

    Project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import UsersContainer from "./UsersContainer";
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import Slider from "./Slider";
-
+import Head from "./Head";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Shop from "./Shop";
 
 function App() {
   return (
     <div className="App">
-      <header className='header'>
-        <div className='headLeft'>
-          <img height={150} width={150} alt = 'logo' src = '/picture/logo.png'/>
-          <div>
-            <h3>SneakerShop</h3>
-            <p>Best sneakers for you!</p>
-          </div>
-        </div>
-        <ul className='headRight'>
-          <li>
-            <img width={40} height={40} alt = 'basket' src = '/picture/basket.png'/>
-
-          </li>
-          <li>
-            <img width={40} height={40} alt = 'user' src = '/picture/user.png'/>
-          </li>
-        </ul>
-      </header>
-      <div>
-        <Slider/>
-      </div>
-      <div className="content">
-        <div className='text'>Товары</div>
-          <UsersContainer/>
-      </div>
+      <Head />
+      <Routes>
+        <Route path="" element={<Login />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
     </div>
   );
 }
